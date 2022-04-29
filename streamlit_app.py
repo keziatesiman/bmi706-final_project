5921896e394e0788cf1733cd9335749f0338a14f
Kezia Irene <irenekezia98@gmail.com>
2022-04-29 02:08:39 -0400

    Create simple visualization

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,112 +1,53 @@
 import altair as alt
 import pandas as pd
 import streamlit as st
-
-### P1.2 ###
-
-# Move this code into `load_data` function {{
-cancer_df = pd.read_csv("https://raw.githubusercontent.com/hms-dbmi/bmi706-2022/main/cancer_data/cancer_ICD10.csv").melt(  # type: ignore
-    id_vars=["Country", "Year", "Cancer", "Sex"],
-    var_name="Age",
-    value_name="Deaths",
-)
-
-pop_df = pd.read_csv("https://raw.githubusercontent.com/hms-dbmi/bmi706-2022/main/cancer_data/population.csv").melt(  # type: ignore
-    id_vars=["Country", "Year", "Sex"],
-    var_name="Age",
-    value_name="Pop",
-)
-
-df = pd.merge(left=cancer_df, right=pop_df, how="left")
-df["Pop"] = df.groupby(["Country", "Sex", "Age"])["Pop"].fillna(method="bfill")
-df.dropna(inplace=True)
-
-df = df.groupby(["Country", "Year", "Cancer", "Age", "Sex"]).sum().reset_index()
-df["Rate"] = df["Deaths"] / df["Pop"] * 100_000
-
-# }}
-
+import os
+from zipfile import ZipFile, Path
+import numpy as np 
 
 @st.cache
 def load_data():
-    ## {{ CODE HERE }} ##
-    df = ...  # remove this line
-    return df
+
+
+    with ZipFile("merged_datasets.zip") as myzip:
+        
+        no1 = myzip.open("merged_datasets/phase_I_test.csv")
+        no2 = myzip.open("merged_datasets/phase_I_valid.csv")
+        no3 = myzip.open("merged_datasets/phase_I_train.csv")
+        no4 = myzip.open("merged_datasets/phase_II_valid.csv")
+        no5 = myzip.open("merged_datasets/phase_II_test.csv")
+        no6 = myzip.open("merged_datasets/phase_II_train.csv")
+        no7 = myzip.open("merged_datasets/phase_III_valid.csv")
+        no8 = myzip.open("merged_datasets/phase_III_test.csv")
+        no9 = myzip.open("merged_datasets/phase_III_train.csv")
+        
+        
+
+    #Now, we can read in the data
+    df = pd.read_csv(eval('no1'))
+
+    for i in range(2,10): 
+        df_temp = pd.read_csv(eval('no'+ str(i)))
+        df = df.append(df_temp, ignore_index=True)
+
+
+    df_merged_grouped = df.groupby(['phase','status']).agg(trials_count=('nct_id', np.size)).reset_index()
+    return df_merged_grouped
 
 
 # Uncomment the next line when finished
 # df = load_data()
 
 ### P1.2 ###
+df_merged_grouped = load_data()
+
+st.write("## Clinical Trials Visualization")
 
 
-st.write("## Age-specific cancer mortality rates")
-
-### P2.1 ###
-# replace with st.slider
-year = 2012
-subset = df[df["Year"] == year]
-### P2.1 ###
-
-
-### P2.2 ###
-# replace with st.radio
-sex = "M"
-subset = subset[subset["Sex"] == sex]
-### P2.2 ###
-
-
-### P2.3 ###
-# replace with st.multiselect
-# (hint: can use current hard-coded values below as as `default` for selector)
-countries = [
-    "Austria",
-    "Germany",
-    "Iceland",
-    "Spain",
-    "Sweden",
-    "Thailand",
-    "Turkey",
-]
-subset = subset[subset["Country"].isin(countries)]
-### P2.3 ###
-
-
-### P2.4 ###
-# replace with st.selectbox
-cancer = "Malignant neoplasm of stomach"
-subset = subset[subset["Cancer"] == cancer]
-### P2.4 ###
-
-
-### P2.5 ###
-ages = [
-    "Age <5",
-    "Age 5-14",
-    "Age 15-24",
-    "Age 25-34",
-    "Age 35-44",
-    "Age 45-54",
-    "Age 55-64",
-    "Age >64",
-]
-
-chart = alt.Chart(subset).mark_bar().encode(
-    x=alt.X("Age", sort=ages),
-    y=alt.Y("Rate", title="Mortality rate per 100k"),
-    color="Country",
-    tooltip=["Rate"],
-).properties(
-    title=f"{cancer} mortality rates for {'males' if sex == 'M' else 'females'} in {year}",
+chart = alt.Chart(df_merged_grouped).mark_bar().encode(
+    x='trials_count',
+    y='phase',
+    color='status'
 )
-### P2.5 ###
 
 st.altair_chart(chart, use_container_width=True)
-
-countries_in_subset = subset["Country"].unique()
-if len(countries_in_subset) != len(countries):
-    if len(countries_in_subset) == 0:
-        st.write("No data avaiable for given subset.")
-    else:
-        missing = set(countries) - set(countries_in_subset)
-        st.write("No data available for " + ", ".join(missing) + ".")
