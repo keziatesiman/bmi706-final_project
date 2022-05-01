b06c05eb4cb482281a6fa6122cf085b6d69c48c7
ninasxiong <ninaxiong11@gmail.com>
2022-04-30 22:58:54 -0400

    added time slider

--- apps/trends.py
+++ apps/trends.py
@@ -1,17 +1,15 @@
-import altair as alt
-import pandas as pd
-import streamlit as st
 import os
 import zipfile
-import numpy as np 
+import pandas as pd
+import numpy as np
+import altair as alt
+import streamlit as st
 from vega_datasets import data
 
 @st.cache
 def load_data():
 
-
-    with zipfile.ZipFile("merged_datasets.zip") as myzip:
-        
+    with zipfile.ZipFile("merged_datasets.zip") as myzip:    
         no1 = myzip.open("merged_datasets/phase_I_test.csv")
         no2 = myzip.open("merged_datasets/phase_I_valid.csv")
         no3 = myzip.open("merged_datasets/phase_I_train.csv")
@@ -22,8 +20,6 @@ def load_data():
         no8 = myzip.open("merged_datasets/phase_III_test.csv")
         no9 = myzip.open("merged_datasets/phase_III_train.csv")
         
-        
-
     #Now, we can read in the data
     df = pd.read_csv(eval('no1'))
 
@@ -31,33 +27,39 @@ def load_data():
         df_temp = pd.read_csv(eval('no'+ str(i)))
         df = df.append(df_temp, ignore_index=True)
 
-
     df_merged_grouped = df.groupby(['phase','status']).agg(trials_count=('nct_id', np.size)).reset_index()
 
     df_merged_grouped3 = df.groupby(['outcome','phase']).agg(trials_count=('nct_id', np.size)).reset_index()
     
-    
-    
     country_df = pd.read_csv('https://raw.githubusercontent.com/hms-dbmi/bmi706-2022/main/cancer_data/country_codes.csv', dtype = {'conuntry-code': str})
     country_df = country_df[['Country', 'country-code']]
     country_df = country_df.replace('United States of America', 'United States')
+
     country_code_df = pd.merge(df, country_df,  how='left', left_on='country', right_on='Country')
+    country_code_df["year"] = pd.DatetimeIndex(country_code_df["study_date"]).year
+    df = country_code_df
+
     df_country_new = country_code_df.groupby(['country','country-code']).agg(trials_count=('nct_id', np.size)).reset_index()
 
+    return df, df_merged_grouped, df_merged_grouped3, df_country_new
 
-
-    
-    return df_merged_grouped,df_merged_grouped3, df_country_new
 
 def app():
 
-    df_merged_grouped, df_merged_grouped3 , df_country_new = load_data()
+    df, df_merged_grouped, df_merged_grouped3 , df_country_new = load_data()
 
-    st.write("## Clinical Trials Visualization")
+    st.write("# Visualizing Trends in Clinical Trials")
+
+    year = st.slider("Year", min(df["year"]), max(df["year"]), 2012)
+    subset = df[df["year"] == year]
+
+    countries = ["Austria","Germany","Iceland","Spain","Sweden","Thailand","Turkey"]
+    countries = st.multiselect("Countries", pd.unique(df_country_new["country"]), countries)
+    subset = subset[subset["country"].isin(countries)]
 
     source = alt.topo_feature(data.world_110m.url, 'countries')
 
-    width = 1000
+    width = 900
     height  = 500
     project = 'equirectangular'
 
@@ -68,9 +70,7 @@ def app():
     ).properties(
         width=width,
         height=height,
-        title="Number of clinical trials per country"
     ).project(project)
-
 
     base = alt.Chart(source
         ).properties( 
@@ -89,18 +89,14 @@ def app():
         tooltip=['trials_count:Q', 'country:N']
         )
     
-    # multi-select countries
-    countries = ["Austria","Germany","Iceland","Spain","Sweden","Thailand","Turkey"]
-    countries = st.multiselect("Countries", pd.unique(df_country_new["country"]), countries)
-    subset = df_country_new[df_country_new["country"].isin(countries)]
-
     chart3 = alt.Chart(subset).mark_bar().encode(
         x="country",
         y="trials_count",
         tooltip=["trials_count"]
     ).properties(
-        title="Number of clinical trials for selected countries"
     )
+
+    st.write("Clinical trials per country")
 
     st.altair_chart(background + chart_rate, use_container_width=True)
     st.altair_chart(chart3, use_container_width=True)
