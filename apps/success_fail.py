a715c281e46815c435d9e86ff878d190db5af0aa
nategreenbaum <nategreenbaum@users.noreply.github.com>
2022-05-01 19:58:10 -0400

    Update success_fail.py

--- apps/success_fail.py
+++ apps/success_fail.py
@@ -9,24 +9,12 @@ from vega_datasets import data
 @st.cache
 def load_data():
 
-    with zipfile.ZipFile("merged_datasets.zip") as myzip:    
-        no1 = myzip.open("merged_datasets/phase_I_test.csv")
-        no2 = myzip.open("merged_datasets/phase_I_valid.csv")
-        no3 = myzip.open("merged_datasets/phase_I_train.csv")
-        no4 = myzip.open("merged_datasets/phase_II_valid.csv")
-        no5 = myzip.open("merged_datasets/phase_II_test.csv")
-        no6 = myzip.open("merged_datasets/phase_II_train.csv")
-        no7 = myzip.open("merged_datasets/phase_III_valid.csv")
-        no8 = myzip.open("merged_datasets/phase_III_test.csv")
-        no9 = myzip.open("merged_datasets/phase_III_train.csv")
-        
+    with zipfile.ZipFile("smaller.zip") as myzip:    
+         no1 = myzip.open("smaller.csv")
+            
     #Now, we can read in the data
     df = pd.read_csv(eval('no1'))
-
-    for i in range(2,10): 
-        df_temp = pd.read_csv(eval('no'+ str(i)))
-        df = df.append(df_temp, ignore_index=True)
-
+    
     df_merged_grouped = df.groupby(['phase','status']).agg(trials_count=('nct_id', np.size)).reset_index()
 
     df_merged_grouped3 = df.groupby(['outcome','phase']).agg(trials_count=('nct_id', np.size)).reset_index()
