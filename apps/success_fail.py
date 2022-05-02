2a9b97cb5136da21a56eec2cb236f541c709b88c
nategreenbaum <nategreenbaum@users.noreply.github.com>
2022-05-01 20:13:31 -0400

    Update success_fail.py

--- apps/success_fail.py
+++ apps/success_fail.py
@@ -9,12 +9,24 @@ from vega_datasets import data
 @st.cache
 def load_data():
 
-    with zipfile.ZipFile("smaller.zip") as myzip:    
-         no1 = myzip.open("smaller.csv")
-            
+    with zipfile.ZipFile("merged_datasets.zip") as myzip:    
+        no1 = myzip.open("merged_datasets/phase_I_test.csv")
+        no2 = myzip.open("merged_datasets/phase_I_valid.csv")
+        no3 = myzip.open("merged_datasets/phase_I_train.csv")
+        no4 = myzip.open("merged_datasets/phase_II_valid.csv")
+        no5 = myzip.open("merged_datasets/phase_II_test.csv")
+        no6 = myzip.open("merged_datasets/phase_II_train.csv")
+        no7 = myzip.open("merged_datasets/phase_III_valid.csv")
+        no8 = myzip.open("merged_datasets/phase_III_test.csv")
+        no9 = myzip.open("merged_datasets/phase_III_train.csv")
+        
     #Now, we can read in the data
     df = pd.read_csv(eval('no1'))
-    
+
+    for i in range(2,10): 
+        df_temp = pd.read_csv(eval('no'+ str(i)))
+        df = df.append(df_temp, ignore_index=True)
+
     df_merged_grouped = df.groupby(['phase','status']).agg(trials_count=('nct_id', np.size)).reset_index()
 
     df_merged_grouped3 = df.groupby(['outcome','phase']).agg(trials_count=('nct_id', np.size)).reset_index()
@@ -25,19 +37,17 @@ def load_data():
 
     country_code_df = pd.merge(df, country_df,  how='left', left_on='country', right_on='Country')
     country_code_df["year"] = pd.DatetimeIndex(country_code_df["study_date"]).year.astype("float")
-    df = country_code_df
+    df = country_code_d
 
     df_country_new = country_code_df.groupby(['country','country-code']).agg(trials_count=('nct_id', np.size)).reset_index()
-    
-    SFbyCountry = df.groupby(['country','country-code','outcome']).agg(trials_count=('nct_id', np.size)).reset_index()
 
-
-    return df, df_merged_grouped, df_merged_grouped3, df_country_new, SFbyCountry
+    return df, df_merged_grouped, df_merged_grouped3, df_country_new
 
 
 def app():
 
-     df, df_merged_grouped, df_merged_grouped3, df_country_new, SFbyCountry = load_data()
+    country_code_df, df_merged_grouped, df_merged_grouped3 , df_country_new = load_data()
+   
 
 
     st.write("## Visualizing Trial Success and Failure")
@@ -49,14 +59,29 @@ def app():
         color='status'
     )
     
-    st.write("## Sucesses and Failures by Country")
+    st.write("## Sucesses and Failures by Phase")
+       
+    ### select country ###
+
+    st.write("## Trends Per Country")
+
+    countries = ["Austria","Germany","Iceland","Spain","Sweden","Thailand","Turkey"]
+    countries = st.multiselect("Countries", pd.unique(df_country_new["country"]), countries)
+    subset = subset[subset["country"].isin(countries)]
+
     
-     chart2 = alt.Chart(SFbyCountry).mark_bar().encode(
-        x=alt.X('sum(trials_count)', stack="normalize", axis=alt.Axis(format='%', title='percentage')),
-        y='country',
+   
+    SFbyCountry = df.groupby(['country','country-code','outcome']).agg(trials_count=('nct_id', np.size)).reset_index()
+
+
+    ### bar chart ###
+
+    chart2 = alt.Chart(SFbyCountry).mark_bar().encode(
+        x=alt.X('country', stack="normalize", axis=alt.Axis(format='%', title='percentage')),
+        y='phase',
         color='outcome'
     )
- 
+    
 
     st.altair_chart(chart1, use_container_width=True)
     st.altair_chart(chart2)
