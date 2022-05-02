71c6ff29c73a200950ba4aa76a0beca4f27981a9
nategreenbaum <nategreenbaum@users.noreply.github.com>
2022-05-02 01:10:11 -0400

    Update success_fail.py

--- apps/success_fail.py
+++ apps/success_fail.py
@@ -34,22 +34,24 @@ def load_data():
     countries = ["Austria","Germany","Iceland","Spain","Sweden","Thailand","Turkey"]
     SFbyCountry = SFbyCountry[SFbyCountry["country"].isin(countries)]
     #####
-    df = pd.read_csv(eval('no1'))
+    #df = pd.read_csv(eval('no1'))
     
-    success_count = df[df.outcome == 1]
-    success_count = success_count[success_count.participant_count  > 0]
+    #success_count = df[df.outcome == 1]
+    #success_count = success_count[success_count.participant_count  > 0]
     
-    fail_count = df[df.outcome == 0]
-    fail_count = fail_count[fail_count.participant_count  > 0]
+    #fail_count = df[df.outcome == 0]
+    #fail_count = fail_count[fail_count.participant_count  > 0]
     
     ####
     
-    return df, df_merged_grouped, df_merged_grouped3, df_country_new, SFbyCountry, success_count, fail_count
+    #return df, df_merged_grouped, df_merged_grouped3, df_country_new, SFbyCountry, success_count, fail_count
+    return df, df_merged_grouped, df_merged_grouped3, df_country_new, SFbyCountry
 
 
 def app():
 
-    country_code_df, df_merged_grouped, df_merged_grouped3 , df_country_new, SFbyCountry, success_count, fail_count = load_data()
+    #country_code_df, df_merged_grouped, df_merged_grouped3 , df_country_new, SFbyCountry, success_count, fail_count = load_data()
+    country_code_df, df_merged_grouped, df_merged_grouped3 , df_country_new, SFbyCountry = load_data()
    
 
 
