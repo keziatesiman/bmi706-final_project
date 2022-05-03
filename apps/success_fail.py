817f9dc3e93f031c0afed353c5b46cd051964a32
nategreenbaum <nategreenbaum@users.noreply.github.com>
2022-05-03 15:47:56 -0400

    Update success_fail.py

--- apps/success_fail.py
+++ apps/success_fail.py
@@ -80,6 +80,11 @@ def app():
         color='status'
     )
     
+    chart1_2 = alt.Chart(df_merged_grouped).mark_bar().encode(
+        x=alt.X('sum(trials_count)', stack="normalize", axis=alt.Axis(format='%', title='percentage')),
+        y='phase',
+        color='outcome'
+    )
     
     ########
 
@@ -126,6 +131,7 @@ def app():
     )
     
     #######
+    st.altair_chart(chart1, use_container_width=True)
     st.altair_chart(chart1, use_container_width=True)
     st.write("## Where do trials fail?")
     st.altair_chart(chart2, use_container_width=True)
